61d74b7c0a14e0d599277c35ede8a017a927ad45
Rakha112 <rakhawibowo1998@gmail.com>
2022-06-23 16:09:54 +0700

    Change the DB setting for Heroku

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -2,9 +2,9 @@ import "dotenv/config";
 import mysql from "mysql";
 
 export const db = mysql.createPool({
-  host: process.env.HOST_DB,
-  user: process.env.USER_DB,
-  password: process.env.PASSWORD_DB,
-  database: process.env.SERVER_DB,
+  host: process.env.HOST_DB_HEROKU,
+  user: process.env.USER_DB_HEROKU,
+  password: process.env.PASSWORD_DB_HEROKU,
+  database: process.env.DATABASE_DB_HEROKU,
   port: 3306,
 });
